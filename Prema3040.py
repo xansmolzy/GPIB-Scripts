65da8b7dd9e8fbd166be536f58d741d09ca58c4a
xansmolzy <61407936+xansmolzy@users.noreply.github.com>
2022-07-21 22:41:41 +0200

    Update Prema3040.py

--- Prema3040.py
+++ Prema3040.py
@@ -11,27 +11,26 @@ inst1 = GPIB.open_resource(7)
 inst1.query("R3")     #Resistance mode 3, PT100 1mA
 inst1.query("F2")     #Auto filter
 inst1.query("T7")     #Set integration time 4S
+inst1.query("CN0")    #Only data on query
 
 inst1.query("O4")     #4 Wire
 inst1.query("U1")     #Basic unit ON
 
-inst1.query("AZA1")    #Auto zero on
-inst1.query("AZT0300") #Auto zero every 300seconds
+inst1.query("AZA1")   #Auto zero on
+inst1.query("AZT0300")#Auto zero every 300seconds
 
-inst1.query("MAR")     #Front, Channel A, RTD
-
+inst1.query("MAR")    #Front, Channel A, RTD
+time.sleep(20)
 while(1):
     try:
-        inst1.query("S1")     #Output format short
         counter = 0
-        a = inst1.read()
-        prevA = a
+        a = inst1.query("RD?") #Query a measurement
+        prevA = a 
         while(a == prevA and counter < 50):
-                a = inst1.read()
+                a = inst1.query("RD?")
                 time.sleep(0.1)
                 counter = counter + 1
-        print(a)
-        print((float(a[:10])-100)/0.385)
+        print(round((float(a[:10])-100)/0.385,4))
     except:
         print("error")
         time.sleep(1)
